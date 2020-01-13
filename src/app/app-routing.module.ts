af9e1342aaef8d56fc601c00d96780a2d584d568
Keerti Kotaru <kvkirthy@gmail.com>
2020-01-13 09:11:42 -0500

    Hash routing so that it works on githup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [ ...pages,
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
