462516d6061dfeb896ad237fed8f109af97eefd7
vencki <kvkirthy@gmail.com>
2020-07-15 20:07:47 -0400

    Newspaper register, login and basic create-post components created.

--- src/app/newspaper/newspaper-routing.module.ts
+++ src/app/newspaper/newspaper-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 const routes: Routes = [{
-  path : 'newspaper',
-  component : HomeComponent
-}];
+  path: 'newspaper',
+  redirectTo: 'newspaper/admin/register',
+  pathMatch: 'full'
+}, {
+  path: 'newspaper',
+  component: HomeComponent,
+  children: [{
+    path: 'admin/register',
+    component: RegisterComponent
+  }, {
+    path: 'admin/login',
+    component: LoginComponent
+  },{
+    path: 'admin/post',
+    component: CreatePostComponent
+  }]
+},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/newspaper/newspaper.module.ts
+++ src/app/newspaper/newspaper.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { NewspaperRoutingModule } from './newspaper-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { NewspaperRoutingModule } from './newspaper-routing.module';
+import { LoginComponent } from './login/login.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RegisterComponent, LoginComponent, CreatePostComponent],
   imports: [
+    FormsModule,
     CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
     NewspaperRoutingModule
   ]
 })
